19eb8cc60b4fe5ef4e492bcd718be1911aceb556
Algorsky <dhy4568527913@163.com>
2026-03-28 14:40:35 +0800

    精英敌人生成子弹

--- src/edu/hitsz/aircraft/MobEnemy.java
+++ src/edu/hitsz/aircraft/MobEnemy.java
@@ -2,6 +2,7 @@ package edu.hitsz.aircraft;
 
 import edu.hitsz.application.Main;
 import edu.hitsz.bullet.BaseBullet;
+import edu.hitsz.bullet.EnemyBullet;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -12,6 +13,15 @@ import java.util.List;
  * @author hitsz
  */
 public class MobEnemy extends AbstractAircraft {
+
+    //每次射击发射子弹数量
+    private int shootNum = 1;
+
+    //子弹威力
+    private int power = 1;
+
+    //子弹射击方向 (向上发射：-1，向下发射：1)
+    private int direction = 1;
 
     public MobEnemy(int locationX, int locationY, int speedX, int speedY, int hp) {
         super(locationX, locationY, speedX, speedY, hp);
@@ -28,7 +38,19 @@ public class MobEnemy extends AbstractAircraft {
 
     @Override
     public List<BaseBullet> shoot() {
-        return new LinkedList<>();
+        List<BaseBullet> res = new LinkedList<>();
+        int x = this.getLocationX();
+        int y = this.getLocationY() + direction*2;
+        int speedX = 0;
+        int speedY = this.getSpeedY() + direction*5;
+        BaseBullet bullet;
+        for(int i=0; i<shootNum; i++){
+            // 子弹发射位置相对飞机位置向前偏移
+            // 多个子弹横向分散
+            bullet = new EnemyBullet(x + (i*2 - shootNum + 1)*10, y, speedX, speedY, power);
+            res.add(bullet);
+        }
+        return res;
     }
 
 }
