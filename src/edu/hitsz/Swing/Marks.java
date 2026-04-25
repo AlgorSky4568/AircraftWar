660337cdd1b5c491aa6fdc2bb85ac9e7c40dc20a
Algorsky <dhy4568527913@163.com>
2026-04-25 12:03:08 +0800

    用swing初步实现按钮和列表

---
+++ src/edu/hitsz/Swing/Marks.java
@@ -0,0 +1,11 @@
+package edu.hitsz.Swing;
+
+import javax.swing.*;
+
+public class Marks {
+    private JTable table1;
+    private JScrollPane scoreTable;
+    private JButton deleteRecordButton;
+    private JLabel difficultyLabel;
+    private JLabel rankingLAbel;
+}
---
+++ src/edu/hitsz/Swing/Start.java
@@ -0,0 +1,33 @@
+package edu.hitsz.Swing;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Start {
+    private JPanel mainPanel;
+    private JButton simpleButton;
+    private JButton commonButton;
+    private JButton difficultButton;
+
+    public Start() {
+        simpleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        commonButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        difficultButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
